19f8d3f4c88349f97bdf6bc5d78f3915aa614a9b
yash-jagtap1706 <115545228+yash-jagtap1706@users.noreply.github.com>
2022-10-23 14:56:18 +0530

    Calculator using python
    Simple Calculator using python language

---
+++ calci.py
@@ -0,0 +1,50 @@
+def add(num1, num2):
+	return num1 + num2
+
+def subtract(num1, num2):
+	return num1 - num2
+
+def multiply(num1, num2):
+	return num1 * num2
+
+def divide(num1, num2):
+	return num1 / num2
+
+loop=1
+while loop==1:
+ print("Please select operation -\n" \
+    "1. Add\n"\
+    "2. Subtract\n" \
+    "3. Multiply\n"  \
+    "4. Divide\n" \
+    "5. Exit")
+
+ try:
+  select = int(input("Enter any option:"))
+  if(select==5):
+      loop=0
+  elif(select>5) :
+      print("Invalid instructions!\n Do you want to try again?")      
+  else:
+    number1 = int(input("Enter first number: "))
+    number2 = int(input("Enter second number:"))
+  
+  if (select == 1):
+   print(number1, "+", number2, "=",add(number1, number2))
+
+  elif (select == 2):
+   print(number1, "-", number2, "=",subtract(number1, number2))
+
+  elif (select == 3):
+   print(number1, "*", number2, "=",multiply(number1, number2))
+
+  elif (select == 4):
+
+   if (number2 == 0):
+       print("[Sorry, the answer is not defined]\n")
+   else:
+       print(number1, "/", number2, "=",divide(number1, number2))
+  
+  
+ except:
+    print("[error, this is IMPOSSIBLE!\n Do you want to try again?]\n")
